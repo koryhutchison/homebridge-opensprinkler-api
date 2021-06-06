41ce36c48fe8bb93364f8be2198ced0e2f58c9ac
Kory Hutchison <kory.hutchison@icloud.com>
2021-06-05 19:23:00 -0500

    Added state management and more robust error handling

--- src/rainDelay.ts
+++ src/rainDelay.ts
@@ -3,6 +3,8 @@ import { OpenSprinklerPlatform } from './platform';
 import { OpenSprinklerApi } from './openSprinklerApi';
 
 export class RainDelay {
+  status: boolean;
+
   constructor(
     private readonly platform: OpenSprinklerPlatform,
     private readonly service: Service,
@@ -11,19 +13,34 @@ export class RainDelay {
     this.service.setCharacteristic(this.platform.Characteristic.Name, 'Rain Delay');
     this.service.setCharacteristic(this.platform.Characteristic.On, false);
     this.service.getCharacteristic(platform.Characteristic.On).onSet(this.setOn.bind(this));
+
+    // Set the inital state to be off
+    this.status = false;
   }
 
   async setOn(value: CharacteristicValue) {
-    if (value) {
-      await this.openSprinklerApi.setRainDelay(this.platform.config.rainDelay);
-    } else {
-      // Setting the rain delay to 0 cancels it
-      await this.openSprinklerApi.setRainDelay(0);
+    try {
+      if (value) {
+        await this.openSprinklerApi.setRainDelay(this.platform.config.rainDelay);
+        this.status = true;
+      } else {
+        // Setting the rain delay to 0 cancels it
+        await this.openSprinklerApi.setRainDelay(0);
+        this.status = false;
+      }
+    } catch (error) {
+      this.platform.log.error(error);
     }
+
+    this.service.updateCharacteristic(this.platform.Characteristic.On, value);
   }
 
   updateOnState(value: boolean) {
-    this.platform.log.debug(`Updating the rainDelay with a value of: ${value}`);
-    this.service.updateCharacteristic(this.platform.Characteristic.On, value);
+    // Only update when the status actually changes
+    if (this.status !== value) {
+      this.platform.log.debug(`Updating the rainDelay with a value of: ${value}`);
+      this.service.updateCharacteristic(this.platform.Characteristic.On, value);
+      this.status = value;
+    }
   }
 }
