99a2a36e717bc41b95026d09db62f4f659d636b8
Kory Hutchison <kory.hutchison@icloud.com>
2022-06-05 20:23:01 -0500

    Fixing node imports to work with Node 14

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { OpenSprinklerPlatform } from './platform';
+import { PLATFORM_NAME } from './settings.js';
+import { OpenSprinklerPlatform } from './platform.js';
 
 const main = (api: API) => {
   api.registerPlatform(PLATFORM_NAME, OpenSprinklerPlatform);
